0ee86f3aec095bde49f779b30dd5d3c5e34b11fa
Miguel Angel Riera <marieracamarasa@gmail.com>
2026-09-18 14:10:31 +0200

    Update app_config.dart

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -2,7 +2,7 @@ class AppConfig {
   AppConfig._();
 
   // static const String apiBaseUrl = 'http://88.18.223.106:8080/api';
-  static const String apiBaseUrl = 'http://192.168.1.36:8080/api';
+  static const String apiBaseUrl = 'http://192.168.1.160:8080/api';
 
   static String get mediaBaseUrl {
     final uri = Uri.parse(apiBaseUrl);
