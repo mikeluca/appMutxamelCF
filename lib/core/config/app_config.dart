d85a8d3df9b77330de011a906183a5a5b07861de
Miguel Angel Riera <marieracamarasa@gmail.com>
2026-09-24 16:54:03 +0200

    Update app_config.dart

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,8 +1,9 @@
 class AppConfig {
   AppConfig._();
 
-  static const String apiBaseUrl = 'https://api.mutxamelcf.es/api';
-  // static const String apiBaseUrl = 'http://192.168.1.160:8080/api';//IP LOCAL
+  //static const String apiBaseUrl = 'https://api.mutxamelcf.es/api'; //ENTORNO PRO
+  static const String apiBaseUrl =
+      'http://192.168.1.160:8080/api'; //ENTORNO DEV
 
   static String get mediaBaseUrl {
     final uri = Uri.parse(apiBaseUrl);
